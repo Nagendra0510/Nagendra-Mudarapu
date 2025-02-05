bced251e5d9e3398c51d864c8eec11312b1ce50f
Abhi-0212000 <nannuriabhi2000@gmail.com>
2025-02-05 23:23:51 +0100

    fixed the extra coment being displayed below copyrights

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Portfolio from './App';  // Changed from App to Portfolio
+import Portfolio from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Portfolio />  // Changed from App to Portfolio
+    <Portfolio />
   </React.StrictMode>
 );
 
